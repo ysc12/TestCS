ad4c21141eb510ff12eb9796b9df916c758f7d93
eunji1223 <83539991+eunji1223@users.noreply.github.com>
2022-07-31 17:26:24 +0900

    Action

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        public static event Action myAction;
+        static void Main(string[] args)
+        {
+
+            while(true)
+            {
+                string s1 = Console.ReadLine();
+
+                switch (s1)
+                {
+                    case "1":
+                        myAction += One;
+                        break;
+                    case "2":
+                        myAction += Two;
+                        break;
+                    case "3":
+                        myAction += Three;
+                        break;
+                    case "4":
+                        myAction?.Invoke();
+                        break;
+                    default:
+                        break;
+                }
+                
+                
+                
+            }
+
+
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+
+        static void One()
+        {
+            Console.WriteLine("Hi world");
+        }
+
+        static void Two()
+        {
+            Console.WriteLine("Hello world");
+        }
+
+        static void Three()
+        {
+            Console.WriteLine("안녕");
+        }
+
+    }
+
+}
